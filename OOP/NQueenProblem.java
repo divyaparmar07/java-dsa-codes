53f6f35427bd1fc2c3b8c50191fc570c2ef35f8e
Divya Parmar <109431181+divyaparmar07@users.noreply.github.com>
2026-04-23 09:45:34 +0530

    Added N-Queen Problem Code

--- OOP/NQueenProblem.java
+++ OOP/NQueenProblem.java
@@ -1,10 +1,14 @@
 package OOP;
 
+// three ways 
+// 1. all possible
+// 2. count ways
+// 3. solution is exist or not if yes then print one.
 public class NQueenProblem {
     static int count = 0;
     public static void main(String[] args) {
         
-        int n = 4; // output 2 boards n = 5 then 10 boards printing
+        int n = 2; // output 2 boards n = 5 then 10 boards printing
         char board[][] = new char[n][n];
         //initialize
         for (int i = 0; i < n; i++) {
@@ -13,8 +17,14 @@ public class NQueenProblem {
             }
         }
 
-        nQueens(board, 0);
-        System.out.println("total ways to solve n queens = " + count);
+        // nQueens(board, 0);
+        if (nQueens(board, 0)) {
+            System.out.println("solution is possible");
+            printBoard(board);
+        } else {
+            System.out.println("Solution is not possible");
+        }
+        // System.out.println("total ways to solve n queens = " + count);
     }
 
     public static boolean isSafe(char board[][], int row, int col) {
@@ -42,22 +52,44 @@ public class NQueenProblem {
     }
     
     // Q3 - N Queens - Time Complexity O(n!)
-    public static void nQueens(char board[][], int row) {
+    // public static void nQueens(char board[][], int row) {
+    //     //base
+    //     if (row == board.length) {
+    //         // printBoard(board);
+    //         count++; // couting possible ways
+    //         return;
+    //     }
+
+    //     //column loop
+    //     for (int j = 0; j < board.length; j++) {
+    //         if (isSafe(board, row, j)) {
+    //             board[row][j] = 'Q';
+    //             nQueens(board, row+1); // function call
+    //             board[row][j] = '.'; // backtracking step
+    //         }
+    //     }
+    // }
+
+    // Check if problem can be solved & print only 1 solution to N Queens Problem
+    public static boolean nQueens(char board[][], int row) {
         //base
         if (row == board.length) {
             // printBoard(board);
             count++; // couting possible ways
-            return;
+            return true;
         }
 
         //column loop
         for (int j = 0; j < board.length; j++) {
             if (isSafe(board, row, j)) {
                 board[row][j] = 'Q';
-                nQueens(board, row+1); // function call
+                if (nQueens(board, row+1)) {
+                    return true;
+                } 
                 board[row][j] = '.'; // backtracking step
             }
         }
+        return false; // not any one possible way
     }
 
     public static void printBoard(char board[][]) {
