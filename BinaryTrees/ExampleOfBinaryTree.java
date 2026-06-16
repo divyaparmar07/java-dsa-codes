9e5c03acba39800ff10a4d9752786699b4dd15f3
Divya Parmar <109431181+divyaparmar07@users.noreply.github.com>
2026-06-16 21:57:17 +0530

    Add files via upload

--- BinaryTrees/ExampleOfBinaryTree.java
+++ BinaryTrees/ExampleOfBinaryTree.java
@@ -1,5 +1,6 @@
 package BinaryTrees;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -169,6 +170,88 @@ public class ExampleOfBinaryTree {
         System.out.println();
     }
 
+    public static void KLevel(Node root, int level, int k) {
+        if (root == null) {
+            return;
+        }
+        if (level == k) {
+            System.out.print(root.data + " ");
+            return;
+        }
+
+        KLevel(root.left, level+1, k);
+        KLevel(root.right, level+1, k);
+    }
+
+    public static boolean getPath(Node root, int n, ArrayList<Node> path) {
+        // base condition
+        if (root == null) {
+            return false;
+        }
+        // add node
+        path.add(root);
+
+        if (root.data == n) {
+            return true;
+        }
+
+        boolean foundLeft = getPath(root.left, n, path);
+        boolean foundRight = getPath(root.right, n, path);
+
+        // checking for if node is found in subtree left and right
+        if (foundLeft || foundRight) {
+            return true;
+        }
+
+        // removing current root
+        path.remove(path.size()-1);
+        return false;
+    }
+
+    // Time complexity = O(n), Space complexity = O(n)
+    public static Node lca(Node root, int n1, int n2) {
+        // 1. finding path
+        ArrayList<Node> path1 = new ArrayList<>();
+        ArrayList<Node> path2 = new ArrayList<>();
+
+        getPath(root, n1, path1); // O(n)
+        getPath(root, n2, path2); // O(n)
+
+        // last common ancestor
+        int i = 0;
+        for (; i < path1.size() && i <path2.size(); i++) { // O(n)
+            if (path1.get(i) != path2.get(i)) {
+                break;
+            }
+        }
+
+        // last equal node -> i-1th
+        Node lca = path1.get(i-1);
+        return lca;
+    }
+
+    public static Node lca2(Node root, int n1, int n2) {
+        // base condition
+        if (root == null || root.data == n1 || root.data == n2) {
+            return root;
+        }
+        
+        Node leftLca = lca2(root.left, n1, n2);
+        Node rightLca = lca2(root.right, n1, n2);
+
+        // leftLCA = val   rightLCA = null
+        if (rightLca == null) {
+            return leftLca;
+        }
+        // leftLCA = null   rightLCA = val
+        if (leftLca == null) {
+            return rightLca;
+        }
+
+        // both values are valid then return root
+        return root;
+    }
+
     public static void main(String[] args) {
         /*
                     1
@@ -202,6 +285,11 @@ public class ExampleOfBinaryTree {
 
         // System.out.println(isSubtree(root, subRoot));
 
-        topView(root);
+        // topView(root);
+        // KLevel(root, 1, 3);
+
+        int n1 = 4, n2 = 6;
+        // System.out.print(lca(root, n1, n2).data);
+        System.out.print(lca2(root, n1, n2).data);
     }
 }
