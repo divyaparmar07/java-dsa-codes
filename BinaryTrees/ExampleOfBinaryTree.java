fedebba9053fe43e60da773e67854cedb4310229
Divya Parmar <109431181+divyaparmar07@users.noreply.github.com>
2026-06-20 21:32:31 +0530

    Add files via upload

--- BinaryTrees/ExampleOfBinaryTree.java
+++ BinaryTrees/ExampleOfBinaryTree.java
@@ -1,5 +1,6 @@
 package BinaryTrees;
 
+import java.lang.annotation.Retention;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -252,6 +253,83 @@ public class ExampleOfBinaryTree {
         return root;
     }
 
+    public static int lcaDist(Node root, int n) {
+        if (root == null) {
+            return -1;
+        }
+
+        if (root.data == n) {
+            return 0;
+        }
+        
+        int leftDist = lcaDist(root.left, n);
+        int rightDist = lcaDist(root.right, n);
+
+        if (leftDist == -1 && rightDist == -1) {
+            return -1;
+        } else if (leftDist == -1) {
+            return rightDist + 1;
+        } else {
+            return leftDist + 1;
+        }
+    }
+
+    public static int minDist(Node root, int n1, int n2) {
+        Node lca = lca2(root, n1, n2);
+        int dist1 = lcaDist(lca, n1);
+        int dist2 = lcaDist(lca, n2);
+        return dist1 + dist2;
+    }
+
+    public static int KAncestor(Node root, int n, int k) {
+        if (root == null) {
+            return -1;
+        }
+
+        if (root.data == n) {
+            return 0;
+        }
+
+        int leftDist = KAncestor(root.left, n, k);
+        int rightDist = KAncestor(root.right, n, k);
+
+        if (leftDist == -1 && rightDist == -1) {
+            return -1;
+        }
+
+        int max = Math.max(leftDist, rightDist);
+        if (max + 1 == k) {
+            System.out.println(root.data);
+        }
+        return max + 1;
+    }
+
+    public static int transform(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftChild = transform(root.left);
+        int rightChild = transform(root.right);
+
+        int data = root.data;
+
+        int newLeft = root.left == null ? 0 : root.left.data;
+        int newRight = root.right == null ? 0 : root.right.data;
+        root.data = newLeft + leftChild + newRight + rightChild;
+        return data;
+    }
+
+    public static void preorder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
     public static void main(String[] args) {
         /*
                     1
@@ -288,8 +366,25 @@ public class ExampleOfBinaryTree {
         // topView(root);
         // KLevel(root, 1, 3);
 
-        int n1 = 4, n2 = 6;
+        // int n1 = 4, n2 = 6;
         // System.out.print(lca(root, n1, n2).data);
-        System.out.print(lca2(root, n1, n2).data);
+        // System.out.print(lca2(root, n1, n2).data);
+
+        // int n1 = 4, n2 = 6;
+        // System.out.println(minDist(root, n1, n2));
+
+        // int n = 5, k = 2;
+        // KAncestor(root, n, k);
+
+        /*
+                    27
+                   / \
+                  9   13
+                 / \ /  \
+                0  0 0   0
+        */
+
+        transform(root);
+        preorder(root);
     }
 }
