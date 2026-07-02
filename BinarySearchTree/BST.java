f775b71ed4a353bdaac874e2fe217106b9d6ed45
Divya Parmar <109431181+divyaparmar07@users.noreply.github.com>
2026-07-02 14:46:56 +0530

    Add files via upload

--- BinarySearchTree/BST.java
+++ BinarySearchTree/BST.java
@@ -1,5 +1,7 @@
 package BinarySearchTree;
 
+import java.util.ArrayList;
+
 public class BST {
     static class Node {
         int data;
@@ -112,6 +114,68 @@ public class BST {
         }
     }
 
+    public static void printRoot2Leaf(Node root, ArrayList<Integer> path) {
+        if (root == null) {
+            return;
+        }
+        path.add(root.data);
+        if (root.left == null && root.right == null) {
+            printPath(path);
+        }
+        printRoot2Leaf(root.left, path);
+        printRoot2Leaf(root.right, path);
+        path.remove(path.size()-1); // removing current root
+    }
+
+    public static void printPath(ArrayList<Integer> path) {
+        for (int i = 0; i < path.size(); i++) {
+            System.out.print(path.get(i) + "->");
+        }
+        System.out.println("Null");
+    }
+
+    public static boolean isValidBST(Node root, Node min, Node max) {
+        if (root == null) {
+            return true;
+        }
+
+        if (min != null && root.data <= min.data) {
+            return false;
+        }
+
+        else if (max != null && root.data >= max.data) {
+            return false;
+        }
+
+        return isValidBST(root.left, min, root) && isValidBST(root.right, root, max);
+    }
+
+    //Time Complexity  - O(n)
+    public static Node createMirror(Node root) {
+        // base case
+        if (root == null) {
+            return null;
+        }
+
+        Node leftMirror = createMirror(root.left);
+        Node rightMirror = createMirror(root.right);
+
+        root.left = rightMirror;
+        root.right = leftMirror;
+
+        return root;
+    }
+
+    public static void preorder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
     public static void main(String[] args) {
         // 1 -> 10
         // int values[] = {5, 1, 3, 4, 2, 7};
@@ -129,15 +193,17 @@ public class BST {
         //     System.out.println("no found");
         // }
 
-        int values[] = {8, 5, 3, 1, 4, 6, 10, 11, 14};
+        // int values[] = {8, 5, 3, 1, 4, 6, 10, 11, 14};
+        // int values[] = {1,1,1};
+        // int values[] = {8, 5, 3, 6, 10, 11, 14};
 
-        Node root = null;
-        for (int i = 0; i < values.length; i++) {
-            root = insert(root, values[i]);
-        }
+        // Node root = null;
+        // for (int i = 0; i < values.length; i++) {
+        //     root = insert(root, values[i]);
+        // }
 
-        inorder(root);
-        System.out.println();
+        // inorder(root);
+        // System.out.println();
 
         // root = delete(root, 5);
         // System.out.println();
@@ -145,6 +211,42 @@ public class BST {
         // inorder(root);
         // System.out.println();
 
-        printInRange(root, 5, 12);
+        // printInRange(root, 5, 12);
+
+        // printRoot2Leaf(root, new ArrayList<>());
+
+        // if (isValidBST(root, null,null)) {
+        //     System.out.println("valid");
+        // } else {
+        //     System.out.println("not valid");
+        // }
+
+        /*
+                        8
+                       / \
+                      5   10
+                     / \    \
+                    3   6   11
+        */
+
+        Node root = new Node(8);
+        root.left = new Node(5);
+        root.right = new Node(10);
+        root.left.left = new Node(3);
+        root.left.right = new Node(6);
+        root.right.right = new Node(11);
+
+
+         /*
+                        8
+                       / \
+                      10  5
+                     /   / \
+                    11  6   3
+                    Mirror BST
+        */
+
+        root = createMirror(root);
+        preorder(root);
     }
 }
